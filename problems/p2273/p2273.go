f9803ce15979fca66f0b5bba26f02394ec45a0e0
Alexey Lazarev <aleksey.lazarev@hotmail.com>
2022-05-24 17:09:18 +0300

    update for problem N2273
    - move from value types to reference types

--- problems/p2273/p2273.go
+++ problems/p2273/p2273.go
@@ -2,12 +2,10 @@ package p2273
 
 func removeAnagrams(words []string) []string {
 	l := len(words)
-	buffer := make(map[string][26]int, l)
+	buffer := make(map[string]*[26]int, l)
 	for i := l - 1; i >= 1; i-- {
-		hash := getHash(words[i], buffer)
-		phash := getHash(words[i-1], buffer)
 
-		if isHashEquals(hash, phash) {
+		if isHashEquals(words[i], words[i-1], &buffer) {
 			words = append(words[:i], words[i+1:]...)
 		}
 	}
@@ -15,27 +13,28 @@ func removeAnagrams(words []string) []string {
 	return words
 }
 
-func isHashEquals(h1 [26]int, h2 [26]int) bool {
+func isHashEquals(str1 string, str2 string, buffer *map[string]*[26]int) bool {
+	pH1 := getHash(str1, buffer)
+	pH2 := getHash(str2, buffer)
 	for i := 0; i < 26; i++ {
-		if h1[i] != h2[i] {
+		if pH1[i] != pH2[i] {
 			return false
 		}
 	}
 	return true
 }
 
-func getHash(str string, buffer map[string][26]int) [26]int {
-	h, ok := buffer[str]
-
+func getHash(str string, buffer *map[string]*[26]int) *[26]int {
+	ib := *buffer
+	h, ok := ib[str]
 	if ok {
 		return h
 	}
-
 	hash := [26]int{}
 	for _, c := range str {
 		hash[c-'a']++
 	}
 
-	buffer[str] = hash
-	return hash
+	ib[str] = &hash
+	return &hash
 }
