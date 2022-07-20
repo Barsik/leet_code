b70ad084f352e7c0e21f25f6618bc89c5c0f2446
Barsik <aleksey.lazarev@hotail.com>
2022-07-20 17:32:26 +0300

    226. Invert Binary Tree

---
+++ problems/p226/p226.go
@@ -0,0 +1,20 @@
+package p226
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	left := root.Left
+	right := root.Right
+
+	root.Right = invertTree(left)
+	root.Left = invertTree(right)
+
+	return root
+}
